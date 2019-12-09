0de47cc54921f6bc4e39b72932777606a7a88b21
leenahabi <leenaalnahabi@gmail.com>
2019-12-09 06:31:57 +0300

    wk04day01hw

--- solution.rb
+++ solution.rb
@@ -16,25 +16,13 @@
       ]
       
       upper_case_full_names = []
-    
-      ## [ 'JOHN DOE', 'JANE DOE', 'JENNIFER ANISTON' ]
-    
-    # Q_1
-    #   ```
-    #   ########The solution by using map method#######
-    #     new_arr = students.map do |element|
-    #         element[:first_name] + " " + element[:last_name]
-    #     end
-    #     p new_arr
-    #   ```
-    #   ```
-    #   #######The solution by using .each loop#######
-    #   new_arr = [] ##create new empty array
-    #   students.each do |element|
-    #     upper_case_full_names.push(element.first_name.upcase + " " + element.last_name.upcase)
-    #   end
-    #   p new_arr
-    # ```
+      upper_case_full_names = students.map do |name| 
+       " #{name[:first_name].upcase} #{name[:last_name].upcase}" 
+       end
+       p upper_case_full_names
+
+
+       
     
     #####          Q2      ########
     users = [
@@ -76,27 +64,12 @@
         }
       ]
       
-      first_order_for_each_user = []
-    
-    
-    
-    # Q_2
-    # ```
-    #   #######The solution by using map method#######
-    #   first_order_for_each_user = users.map do |order|
-    #     "#{order[:orders].first}"
-    #   end
-    #   puts first_order_for_each_user
-    #   ```
-    
-    #   ```
-    #   #The solution by using .each loop
-    #   users.each do |element|
-    #     first_order_for_each_user.push(element[:orders].first)
-    #   end
-    #   p first_order_for_each_user
-    # ```
-    
+      first_order_for_each_user = users.map do |order| 
+       " #{order[:orders][0]} " 
+       end
+       p first_order_for_each_user
+   
+       
     
     #####          Q3      ########
     people = [
@@ -152,51 +125,26 @@
             ]
         }
       ]
-      
-      
-      coffee_average_per_person = []
-    
-    # Q_3
-    # ```
-    #   #######The solution by using .each loop#######
-    # people.each do |hash|  ##first loop for hash 
-    #     new_person = {name:"",coffee_average:0}
-    #     new_person[:name] = hash[:name]
-    #     puts hash[:name]
-    #     counter = 0
-    #     total_amout = 0
-    #     hash[:transactions].each do |key,value|  ##second loop for transcations [{},{},{}]
-    #         if key[:type] == "COFFEE"
-    #             total_amout += key[:amount]
-    #             counter += 1
-    #         end
+      avrage = 0
+      sum = 0
+    people.each do |name|
+      names =  name[:name]
+    end
+    people.each do |trans|
+    transa = trans[:transactions]  
+     transa.each do |avg|
+         if avg[:type] == "COFFEE"
+          sum =  avg[:amount] + avrage
+         end   
+    end 
+    end
+    #   coffee_average_per_person = []
+    #         case people[:transactions]
+    #         when type = "COFFEE"
+    #             puts 
     #     end
-    #     new_person[:coffee_average] = total_amout / counter
-    #     coffee_average_per_person.push(new_person)
-    #   end
-    
-    #   p coffee_average_per_person
-    #   ```
-    
-    # ```
-    #  #######The solution by using map method#######
-    # coffee_average_per_person = people.map do |person|
-    
-    #     coffee_sum = []
-    #     person[:transactions].map do |transaction|
-    #         if transaction[:type] == 'COFFEE'
-    #             coffee_sum.push(transaction[:amount])
-    #         end
-    #     end
-        
-    #     { 
-    #     name: person[:name] , 
-    #     coffee_average: coffee_sum.reduce(:+) / coffee_sum.length 
-    #     }
-    # end
-    
-    # puts coffee_average_per_person
-    # ```
+
+
     
     #####          Q4      ########
     stores = [
@@ -240,47 +188,19 @@
             ]
         }
       ]
-      
-      
+
+     
+      stores.each do |prod|
+      product = prod[:products]  
+       product.each do |highest_price|
+        price = highest_price[:price]
+           if price >= highest_price[:price]
+            new_ex.push(highest_price[:price])
+           end   
+      end 
+      end
+
+
     # Q_4
        
-    # ```
-    #######The solution by using .each loop#######
-    # most_expensive_products_by_store = []
-    
-    # stores.each do |hash|
-    #     new_store_with_expensive_product = {store_name:"",most_expensive_product:{}}
-    #     expensive_product = {description:"",price:0}
-    
-    #     new_store_with_expensive_product[:store_name] = hash[:store_name]
-    #     hash[:products].each do |hash_products|
-    #         if hash_products[:price] > expensive_product[:price]
-    #             expensive_product[:price] = hash_products[:price]
-    #             expensive_product[:description] = hash_products[:description]
-    #         end
-    #     end
-    #     new_store_with_expensive_product[:most_expensive_product] = expensive_product
-    #     most_expensive_products_by_store.push(new_store_with_expensive_product) 
-    # end
-    
-    # puts most_expensive_products_by_store
-    # ```
-    
-    # ```
-    # #######The solution by using map method#######
-    # most_expensive_products_by_store = []
-    # most_expensive_products_by_store = stores.map do |store|
-    #     expensive = store[:products].max_by do |product|
-    #         product[:price]
-    #     end  
-    
-    #     {
-    #        name: store[:store_name] ,
-    #        price: expensive[:price]
-    #     }
-    # end 
-    
-    # puts most_expensive_products_by_store
-    # ```
-    
-    #######
+   
